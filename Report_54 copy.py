d12b71b2139ac3f2812bb17ff8c9b0b68b00882b
karencubas <karenc.cubas@gmail.com>
2024-03-19 10:57:04 -0400

    pushed a document

---
+++ Report_54 copy.py
@@ -0,0 +1,41 @@
+def menu():
+    print("Menu\n-------------\n"
+          "1.Encode\n2.Decode\n3.Quit\n")
+
+def encode(original):
+    encoded = ""
+     ## Shift digits of original up by 3
+    for char in original:
+        if int(char) < 7:
+           encoded += str(int(char) + 3)
+        if int(char) >= 7:
+           encoded += str(int(char) + 3 -10 )
+
+    return encoded
+def decode(encoded):
+    ## Partner
+    decoded = ""
+
+    return decoded
+
+if __name__ == '__main__' :
+
+    while True:
+       menu()
+
+       option = int(input("Please enter an option: "))
+
+       ## Encode password
+       if option == 1:
+           original = input("Please enter your password to encode: ")
+           encoded = encode(original)
+           print("Your password has been encoded and stored!\n")
+
+       ## Decode password
+       if option == 2:
+           decoded = decode(encoded)
+           print(f"The encoded password is {encoded}, and the original password is {decoded}.\n")
+
+       ## Quit program
+       if option == 3:
+           exit()
